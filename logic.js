6302c6df19a62203b10eb3a6b6974abb824361df
Alvin Galit <aroblesgalit@gmail.com>
2020-03-31 11:11:29 -0500

    Started ajax call on button click to get data from superhero api

--- logic.js
+++ logic.js
@@ -22,7 +22,41 @@ $(document).ready(function () {
     // THEN I am presented with the information of another result
     // Get data from superhero api for multiple results
     // Render data on DOM
+    $("button").on("click", function (e) { // Change to target form and submit event when HTML is ready
+        e.preventDefault();
 
+        // Superhero API ---------WORKS!!!        
+        var queryName = "Spider-man";
+        var accessToken = "2839209799538545";
+        var queryURL = "https://superheroapi.com/api/" + accessToken + "/search/" + queryName;
+
+        jQuery.ajaxPrefilter(function (options) {
+            if (options.crossDomain && jQuery.support.cors) {
+                options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+            }
+        });
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.results;
+            if (results.length > 1) {
+                // Create a button for each result
+                // Take data for each result
+                // Only view the first one and hide the rest (through class)
+            }
+            // var searchList = response.results;
+            // for (var i = 0; i < searchList.length; i++) {
+            //     var heroName = searchList[i].name;
+            //     var imgSrc = searchList[i].image.url;
+
+            //     var heroDiv = $("<div>").append($("<h1>").text(heroName)).append($("<img>").attr("src", imgSrc));
+            //     $(".container").append(heroDiv);
+            // }
+
+        })
+    })
 
 
     // Cynthia
