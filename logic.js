cefe8b1a9ef61d68d66a4ffbe9734503d77baeca
Alvin Galit <aroblesgalit@gmail.com>
2020-04-01 15:57:59 -0500

    added Cynthia's logic for getting data from api and rendering images

--- logic.js
+++ logic.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
 
         // Clear heroInfo div
         $("#heroInfo").empty();
-        
+
         // Get input value
         var queryName = $("#heroSearchInput").val().trim();
         var accessToken = "2839209799538545";
@@ -152,6 +152,55 @@ $(document).ready(function () {
             }
 
         })
+        
+        // Cynthia
+        // WHEN I scroll down even more
+        // THEN I am presented with images of wallpapers related to the superhero
+        // Get data from image api
+        // Render data on DOM
+
+        var queryname = $("#heroSearchInput").val().trim();
+        var AccessKey = "JINdia7koUjq_pI2PJaRPDBiIJfg9sGoHF4a3t_2olw";
+        var queryUrl = "https://api.unsplash.com/search/photos/?client_id=" + AccessKey + "&query=" + queryname;
+        
+        jQuery.ajaxPrefilter(function (options) {
+            if (options.crossDomain && jQuery.support.cors) {
+                options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+            }
+        });
+
+        // Ajax  GET request
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+            //after data comes back from API
+        }).then(function (response) {
+            // console.log(response);
+            var result = response.results;
+            console.log(result);
+            // create divs here//
+            var imageContainer = $("<div>").attr('uk-grid', '');
+            var ukimage = $("<div>").addClass('uk-width-expand@m superPics');
+            var h2title = $('<h2>').text('Wallpapers');
+            //Loop
+            for (var i = 0; i < 4; i++) {
+                //console.log(result[i]);
+                var imageURL = result[i].urls.regular;
+                var altdescription = result[i]["alt_description"];
+                // console.log(altdescription);
+                //console.log(imageURL);
+                // image divs
+                var img = $('<img>').attr({ src: imageURL, alt: altdescription })
+                //append img uk
+                ukimage.append(img)
+            }
+            ukimage.prepend(h2title)
+            imageContainer.prepend(ukimage)
+            $('body').append(imageContainer)
+            //$('.image img').attr('src',imageURL );
+        });
+
+
         // Clear the input value
         $("#heroSearchInput").val("");
     })
@@ -169,9 +218,4 @@ $(document).ready(function () {
     })
 
 
-    // Cynthia
-    // WHEN I scroll down even more
-    // THEN I am presented with images of wallpapers related to the superhero
-    // Get data from image api
-    // Render data on DOM
 })
