20b100be9d7d16acd279509d45e49177fe7806b3
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-03-31 19:21:22 -0500

    ajax for wallpaper and API

---
+++ logic.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    // User story
+    // As a superhero fanatic, I want to search for my favorite superhero so I can learn more about them.
+
+    // Acceptance Criteria
+    // GIVEN the application
+    // WHEN I arrive on the home screen
+    // THEN I am presented with an animation telling me about the application
+
+    // Alexandra
+    // WHEN I submit a search query 
+    // THEN the page reloads and views information about the superhero
+    // On form submit, take value of input field
+    // Get data from superhero api for single result
+    // Render data on DOM
+
+    // Alvin
+    // When I receive multiple results for a search query
+    // THEN the page presents me with buttons to browse through them
+    // When I click on a button of a multiple search result
+    // THEN I am presented with the information of another result
+    // Get data from superhero api for multiple results
+    // Render data on DOM
+
+
+
+    // Cynthia
+    // WHEN I scroll down even more
+    // THEN I am presented with images of wallpapers related to the superhero
+    // Get data from image api
+    // Render data on DOM
+})
