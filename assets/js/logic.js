08056bae4616745ab8004519101fd54d150c30ef
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-04-02 11:11:41 -0500

    worked on jsto update in images api

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -183,8 +183,10 @@ $(document).ready(function () {
             // Render data on DOM
 
             // var queryname = $("#heroSearchInput").val().trim(); // Already declared previously
+            var dataName = $("*[data-index='1']").attr("data-name");
+            console.log(dataName);
             var AccessKey = "JINdia7koUjq_pI2PJaRPDBiIJfg9sGoHF4a3t_2olw";
-            var queryUrl = "https://api.unsplash.com/search/photos/?client_id=" + AccessKey + "&query=" + queryName;
+            var queryUrl = "https://api.unsplash.com/search/photos/?client_id=" + AccessKey + "&query=" + dataName;
 
             jQuery.ajaxPrefilter(function (options) {
                 if (options.crossDomain && jQuery.support.cors) {
@@ -211,7 +213,10 @@ $(document).ready(function () {
                     var imageURL = result[i].urls.regular;
                     var altDescription = result[i]["alt_description"];
                     // Crate image divs
+                    
                     var img = $('<img>').attr({ src: imageURL, alt: altDescription, width: "100%" });
+                    //getting data from a div attribute
+
                     //append img uk
                     imageContainer.append(img);
                 }
